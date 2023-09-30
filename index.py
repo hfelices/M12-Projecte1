0f1d5a6237227e8acdfb92bd4c8f46b4d8aa973d
unknown <hfelicesg@fp.insjoaquimmir.cat>
2023-09-30 11:44:31 +0200

    Create, update i delete implementat

--- index.py
+++ index.py
@@ -1,11 +1,65 @@
-from flask import Flask , render_template, redirect, url_for
+from flask import Flask , render_template, flash, redirect, url_for, request
 import sqlite3
-
-
+import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+
 DATABASE = 'database.db'
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def now(): return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + basedir + "/" + DATABASE
+
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy()
+db.init_app(app)
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    photo = db.Column(db.Text)
+    price = db.Column(db.DECIMAL(10, 2), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    seller_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created = db.Column(db.DATETIME, default=now(), nullable=False)
+    updated = db.Column(db.DATETIME, default=now(), onupdate=now(), nullable=False)
+
+    category = db.relationship('Category', backref='products')
+    seller = db.relationship('User', backref='products')
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, unique=True, nullable=False)
+    slug = db.Column(db.Text, unique=True, nullable=False)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, unique=True, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DATETIME, default=now(), nullable=False)
+    updated = db.Column(db.DATETIME, default=now(), onupdate=now(), nullable=False)
+
+    
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
 
 # Comando para iniciar la app flask
 # flask --app .\index.py run
@@ -15,6 +69,7 @@ def hello_world():
     return render_template('hello.html')
 
 
+
 def get_db_connection():
 
     con = sqlite3.connect(DATABASE)
@@ -22,10 +77,91 @@ def get_db_connection():
     con.row_factory = sqlite3.Row
     return con
 
-
-@app.route('/products')
-def item_list():
+def get_table(table):
     with get_db_connection() as con:
-        res = con.execute("SELECT * from products")
-        items = res.fetchall()
-    return render_template('products/list.html', items = items)
+            res = con.execute(f"SELECT * from {table}")
+            items = res.fetchall()
+            return items
+            
+
+@app.route('/products', methods=["GET","POST"])
+def item_list():
+    if request.method == 'GET':
+        items = get_table('products')
+        return render_template('products/list.html', items = items)
+    elif request.method == 'POST':
+        print('POST')
+
+
+@app.route('/products/create', methods=["GET","POST"])
+def create_item():
+    if request.method == 'GET':
+        items = get_table('categories')
+        return render_template('products/create-update-product.html', categories = items)
+    elif request.method == 'POST':
+        d = request.form
+        file = request.files["foto"]
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+        with get_db_connection() as con:
+            cursor = con.cursor()
+            sentencia_sql = '''
+    INSERT INTO products (title, description,photo,price,category_id,seller_id,created,updated)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+'''
+            dades=(d["nombre"], d["descripcion"], file.filename, d["precio"], d["categoria"], '1', now(),now())
+            cursor.execute(sentencia_sql,dades)
+            con.commit()
+            
+
+    return redirect(url_for('item_list'))
+
+
+
+@app.route('/products/update/<int:id>', methods=["GET","POST"])
+def update_item(id):
+    if request.method == 'GET':
+        items = get_table('categories')
+        with get_db_connection() as con:    
+            res = con.execute(f"SELECT * from  products where id =?",(id,))
+            product =  res.fetchone()
+            app.logger.info(product)
+        return render_template('products/create-update-product.html', categories = items, product = product)
+    elif request.method == 'POST':
+        d = request.form
+        if request.files["foto"]:
+            file = request.files["foto"]
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                photo = file.filename
+        else:
+                photo = request.form["photo"]
+        with get_db_connection() as con:
+            cursor = con.cursor()
+            sentencia_sql = f'''
+        UPDATE products
+        SET title = ?, description = ?, photo = ?, price = ?, category_id = ?,
+            seller_id = ?, updated = ?
+        WHERE id = {id}
+    '''
+            dades = (d["nombre"], d["descripcion"], photo, d["precio"], d["categoria"], '1', now())
+            cursor.execute(sentencia_sql,dades)
+            con.commit()
+            
+
+    return redirect(url_for('item_list'))
+
+
+@app.route("/products/delete/<int:id>", methods=["GET","POST"])
+def delete_item(id):
+    product = Product.query.get_or_404(id)
+
+    if request.method == "POST":
+        db.session.delete(product)
+        db.session.commit()
+        return redirect(url_for("item_list"))
+
+    return render_template("/products/list.html")
