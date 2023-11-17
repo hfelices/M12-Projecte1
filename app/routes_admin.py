565f50cbdf2396bd717ef49e6d1be6f2860a8be8
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2023-11-17 15:50:23 +0100

    4.3 blueprint admin

--- app/routes_admin.py
+++ app/routes_admin.py
@@ -21,11 +21,20 @@ admin_bp = Blueprint(
     "admin_bp", __name__, template_folder="templates", static_folder="static"
 )
 
+@admin_bp.route('/admin')
+@login_required
+def admin_index():
+    return render_template('admin/index.html')
 
+@admin_bp.route('/admin/users')
+@login_required
+def admin_users():
+    users = db.session.query(User).all()
+    return render_template('admin/users_list.html', users=users)
 
 # Upload CSV
-@admin_bp.route('/admin', methods=["GET","POST"])
-# @login_required
+@admin_bp.route('/admin/tools', methods=["GET","POST"])
+@login_required
 def admin():
     if request.method == 'POST':
         table = request.form['table']
@@ -59,4 +68,4 @@ def admin():
                 user.password = generate_password_hash("user.password")
             db.session.commit()
         return redirect(url_for('main_bp.admin'))
-    return render_template('admin/admin.html')
+    return render_template('admin/admin_tools.html')
