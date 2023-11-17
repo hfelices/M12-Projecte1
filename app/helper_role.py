5a945ec6952806103ebe69d3bf1d64f68de9a535
unknown <hfelicesg@fp.insjoaquimmir.cat>
2023-11-17 17:18:04 +0100

    estils a index users, i helper_role

---
+++ app/helper_role.py
@@ -0,0 +1,88 @@
+from flask import current_app
+from flask_login import current_user
+from flask_principal import identity_loaded, identity_changed, ActionNeed, RoleNeed, Permission, Identity, AnonymousIdentity
+from enum import Enum
+
+# Custom roles and actions
+class Role(str, Enum):
+    admin = "admin"
+    moderator = "moderator"
+    wanner = "wanner"
+
+class Action(str, Enum):
+    edit = "create, update and delete"
+    view = "list and read"
+    admin = "admin"
+
+# Needs
+__moderator_role_need = RoleNeed(Role.moderator)
+__wanner_role_need = RoleNeed(Role.wanner)
+
+__edit_action_need = ActionNeed(Action.edit)
+__view_action_need = ActionNeed(Action.view)
+
+# Permissions
+require_moderator_role = Permission(__moderator_role_need)
+require_wanner_role = Permission(__wanner_role_need)
+
+require_edit_permission = Permission(__edit_action_need)
+require_view_permission = Permission(__view_action_need)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@identity_loaded.connect
+def on_identity_loaded(sender, identity):
+    identity.user = current_user
+    if hasattr(current_user, 'role'):
+        if current_user.role == Role.moderator:
+            # Role needs
+            identity.provides.add(__moderator_role_need)
+            # Action needs
+            identity.provides.add(__edit_action_need)
+            identity.provides.add(__view_action_need)
+        elif current_user.role == Role.viewer:
+            # Role needs
+            identity.provides.add(__wanner_role_need)
+            # Action needs
+            identity.provides.add(__view_action_need)
+        else:
+            current_app.logger.debug("Unkown role")
+
+def notify_identity_changed():
+    if hasattr(current_user, 'email'):
+        identity = Identity(current_user.email)
+    else:
+        identity = AnonymousIdentity()
+    
+    identity_changed.send(current_app._get_current_object(), identity = identity)
